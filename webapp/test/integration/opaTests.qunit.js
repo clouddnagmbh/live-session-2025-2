591033338cf6526fd0113a94650456bddb9cb56c
Daniel Krancz <daniel.krancz@clouddna.at>
2025-03-18 10:33:10 +0000

    Initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["at/clouddna/gitdemoapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"atclouddna/gitdemoapp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
